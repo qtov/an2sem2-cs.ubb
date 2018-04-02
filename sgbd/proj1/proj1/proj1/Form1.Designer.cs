334c396aa05ac830432ab3d8595ef5346ef5d2d5
rexu <tigrex0806@gmail.com>
2018-04-03 00:21:15 +0300

    Garbage complete. SGBD proj1

---
+++ sgbd/proj1/proj1/proj1/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace proj1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridUsers = new System.Windows.Forms.DataGridView();
+            this.dataGridComments = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridComments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridUsers
+            // 
+            this.dataGridUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridUsers.Location = new System.Drawing.Point(24, 22);
+            this.dataGridUsers.Name = "dataGridUsers";
+            this.dataGridUsers.RowTemplate.Height = 24;
+            this.dataGridUsers.Size = new System.Drawing.Size(672, 733);
+            this.dataGridUsers.TabIndex = 0;
+            this.dataGridUsers.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataGridComments
+            // 
+            this.dataGridComments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridComments.Location = new System.Drawing.Point(720, 22);
+            this.dataGridComments.Name = "dataGridComments";
+            this.dataGridComments.RowTemplate.Height = 24;
+            this.dataGridComments.Size = new System.Drawing.Size(665, 733);
+            this.dataGridComments.TabIndex = 1;
+            this.dataGridComments.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridComments_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1404, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1903, 798);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridComments);
+            this.Controls.Add(this.dataGridUsers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridComments)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridUsers;
+        private System.Windows.Forms.DataGridView dataGridComments;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ sgbd/proj1/proj1/proj1/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proj1
+{
+    public partial class Form1 : Form
+    {
+        private SqlDataAdapter adapterUsers;
+        private SqlDataAdapter adapterComments;
+        private DataSet dataSet;
+        private SqlCommandBuilder cb;
+        private BindingSource bsUsers;
+        private BindingSource bsComments;
+        private SqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection("Data Source=SIBYL2;Initial Catalog=proj1;Integrated Security=True");
+            dataSet = new DataSet();
+            adapterUsers = new SqlDataAdapter("select * from users", conn);
+            adapterComments = new SqlDataAdapter("select * from comments", conn);
+            cb = new SqlCommandBuilder(adapterUsers);
+            adapterUsers.Fill(dataSet, "Users");
+            adapterComments.Fill(dataSet, "Comments");
+
+            DataRelation dr = new DataRelation("FK_comments_users", dataSet.Tables["users"].Columns["id"], dataSet.Tables["comments"].Columns["id_user"]);
+            dataSet.Relations.Add(dr);
+            bsUsers = new BindingSource();
+            bsComments = new BindingSource();
+
+            dataGridUsers.DataSource = bsUsers;
+            dataGridComments.DataSource = bsComments;
+            bsUsers.DataSource = dataSet;
+            bsUsers.DataMember = "users";
+
+            bsComments.DataSource = bsUsers;
+            bsComments.DataMember = "FK_comments_users";
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridComments_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void AddComment(object sender)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Connection is " + conn.State);
+
+                SqlCommandBuilder builder = new SqlCommandBuilder(adapterComments);
+
+                adapterComments.SelectCommand.Connection = conn;
+                adapterComments.Update(dataSet.Tables["comments"]);
+                dataSet.Tables["comments"].Clear();
+                adapterComments.Fill(dataSet, "comments");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+        }
+    }
+}
